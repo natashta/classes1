6d43b22aaaa46448885c8b91d85dcc52b423258e
natashta <natashta@local>
2019-05-06 00:59:53 +0300

    task done

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,57 +1,91 @@
-import getNumber from '../src/js/app';
+import {
+  Boweman, Daemon, Magician, Swordsman, Undead, Zombie,
+} from '../src/js/characters';
 
-test('should return number', () => {
-  const input = '80';
-  const expected = 80;
-  const received = getNumber(input);
-  expect(received).toBe(expected);
+test('create class Boweman', () => {
+  const character = new Boweman('Name');
+  const expected = {
+    name: 'Name',
+    health: 100,
+    level: 1,
+    type: 'Boweman',
+    attack: 25,
+    defence: 25,
+  };
+
+  expect(character).toEqual(expected);
 });
 
-test('input null should return error', () => {
-  const input = '0';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
+test('create class Daemon', () => {
+  const character = new Daemon('Name');
+  const expected = {
+    name: 'Name',
+    health: 100,
+    level: 1,
+    type: 'Daemon',
+    attack: 25,
+    defence: 25,
+  };
+
+  expect(character).toEqual(expected);
 });
 
-test('input negative should return error', () => {
-  const input = '-1';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
+
+test('create class Magician', () => {
+  const character = new Magician('Some name');
+  const expected = {
+    name: 'Some name',
+    health: 100,
+    level: 1,
+    type: 'Magician',
+    attack: 10,
+    defence: 40,
+  };
+
+  expect(character).toEqual(expected);
 });
 
-test('input non integer should return error', () => {
-  const input = '0.6';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
+
+test('create class Swordsman', () => {
+  const character = new Swordsman('Another name');
+  const expected = {
+    name: 'Another name',
+    health: 100,
+    level: 1,
+    type: 'Swordsman',
+    attack: 40,
+    defence: 10,
+  };
+
+  expect(character).toEqual(expected);
 });
 
-test('input string should return error', () => {
-  const input = 'string';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
+
+test('create class Undead', () => {
+  const character = new Undead('One more name');
+  const expected = {
+    name: 'One more name',
+    health: 100,
+    level: 1,
+    type: 'Undead',
+    attack: 40,
+    defence: 10,
+  };
+
+  expect(character).toEqual(expected);
 });
 
-test('empty input should return error', () => {
-  const input = '';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
-});
 
-test('input binary and octagonal numbers should return error', () => {
-  const input = '0234';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
-});
+test('create class Zombie', () => {
+  const character = new Zombie('Another name');
+  const expected = {
+    name: 'Another name',
+    health: 100,
+    level: 1,
+    type: 'Zombie',
+    attack: 10,
+    defence: 40,
+  };
 
-test('empty input hexadecimal number should return error', () => {
-  const input = '0x23';
-  const expected = Error('Ввод некорректен');
-  const received = getNumber(input);
-  expect(received).toEqual(expected);
+  expect(character).toEqual(expected);
 });
